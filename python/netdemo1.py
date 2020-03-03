006e337f004808d278c92587d9f504cc211d1753
gabriele bonanno <gigo.bon@mail.com>
2020-03-03 12:47:15 +0100

    added netdemo1.py

--- python/netdemo1.py
+++ python/netdemo1.py
@@ -1,16 +1,23 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+from keras import backend as K
+
+def custom_activation(x):
+    return (K.sigmoid(x/183) * 100)
+
 model=keras.Sequential([
-	keras.layers.Dense(20,activation=tf.nn.sigmoid, input_shape=[20]),
-	keras.layers.Dense(10,activation=tf.nn.sigmoid),
-	keras.layers.Dense(5,activation=tf.nn.sigmoid),
+	keras.layers.Dense(20,activation=custom_activation, input_shape=[20]),
+	keras.layers.Dense(10,activation=custom_activation),
+	keras.layers.Dense(10,activation=custom_activation),
+	keras.layers.Dense(5,activation=custom_activation),
+	keras.layers.Dense(2,activation=custom_activation),
 	keras.layers.Dense(1, activation=tf.nn.sigmoid)
 ])
 model.compile(optimizer=tf.optimizers.Adam(),loss="binary_crossentropy")
 t_set_x=[]
 t_set_y=[]
-m=10000
+m=100000
 for _ in range(0,m):
 	a=np.random.randint(0,50,7)
 	i=np.random.randint(0,2)
@@ -38,7 +45,7 @@ for _ in range(0,m):
 	v_set_x.append(a)
 	v_set_y.append(i)
 
-model.fit(np.array(t_set_x), np.array(t_set_y), epochs=40)
+model.fit(np.array(t_set_x), np.array(t_set_y), epochs=20)
 model.evaluate(np.array(v_set_x),np.array(v_set_y))
 calssifications=model.predict(np.array(v_set_x))
 print(model.predict(np.array([[14,17,16,3,2,16,15,10,15,17,1,16,8,14,15,19,19,8,12,3]])))
