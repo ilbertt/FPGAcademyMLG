980d0c48d64824f98b45e60ba79906730b08b09e
gabriele bonanno <gigo.bon@mail.com>
2020-03-06 15:23:39 +0100

    added resampling and thresold functions

---
+++ python/audio/functions.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy import signal as sg
+
+"""def resample(sample, s_factor):
+	size=len(sample)
+	sample=sample[0:size-size%s_factor] #n_samples devono essere multipli di s_factor
+	sample=sample.reshape([int(size/s_factor),s_factor])
+	sample=np.ceil(np.array(sum(sample.T))/s_factor)
+	return sample"""
+
+def thresold_resize(sample,thresold, length):
+	arr=list(map(lambda a: True if a>thresold else False, sample))
+	th_index=arr.index(True)
+	if (th_index<len(sample)-lenght):
+		sample=sample[th_index:th_index+length]
+		return sample
+	else:
+		return 0
+
+def resample(sample, s_factor):
+	sample=sg.resample(sample, int(len(sample)/s_factor))
+	return sample
