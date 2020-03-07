4d5a797bc3871ea196834133d4fb79f26a6149c2
Massimo Albarello <albo.max@hotmail.it>
2020-03-07 17:56:03 +0100

    verify keywords ready for the neural net
    double verification: first verify all the "raw" keywords downloaded from firebase, then verify each keyword (after resizing and resampling) ready to train the neural net.
    Also bug fixed in .gitignore and __pycache__ folder added to i

---
+++ python/audio/verify_training_keywords.py
@@ -0,0 +1,27 @@
+# play each keyword ready for training the neural net from local file "true_set.npy"
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sounddevice as sd
+import time
+
+#np.set_printoptions(threshold = sys.maxsize) #print the whole array
+
+fs = 11025
+
+# load keywords in "array" from local "true_set" file
+array = np.load('true_set.npy')
+
+for i in range (0, len(array)):
+	keyword = array[i]
+	print(i)
+	sd.play(keyword, fs)
+	time.sleep(2)	
+	## uncomment to see the fourier transform
+	# KEY = np.fft.fft(keyword)
+	# timestep = 1/fs
+	# freq = np.fft.fftfreq(keyword.size, d=timestep)
+	# plt.plot(freq, np.abs(KEY)) 
+	# plt.title(i)
+	# plt.yscale("log")
+	# plt.show()
