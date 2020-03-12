4b9ac8025a845a71149cb863822469a633274b80
Luca Bertelli <luca.berte98@hotmail.it>
2020-03-12 21:30:35 +0100

    remove deleted keywords
    same as previous commit

--- python/audio/create_true_set.py
+++ python/audio/create_true_set.py
@@ -7,18 +7,26 @@ threshold = 1000	# set threshold for keyword resize [wav sample amplitude]
 fs = 44100			# sample rate
 length = 22000		# significant number of samples for each keyword
 
-# download all .wav files from firebase bucket into true_recs local folder if not already in true_recs directory
+#### download all .wav files from firebase bucket into true_recs local folder if not already in true_recs directory
 path="true_recs"
+current_files=os.listdir(path) #list of keywords currently in local true_recs folder
+
+# list files from Firebase bucket
 fb_files=os.popen("gsutil ls gs://fpgacademy-mlg.appspot.com/true_recs/").read()
 fb_files=fb_files.replace("gs://fpgacademy-mlg.appspot.com/true_recs/","")
 fb_files=fb_files.split('\n')
 fb_files.remove('')
 fb_files.remove('')
-ex_files=os.listdir(path)
+
+#check if there are new keywords to be downloaded from Firebase
 for file in fb_files:
-	if(not(file in ex_files)):
+	if(not(file in current_files)):
 		os.popen("gsutil cp -r gs://fpgacademy-mlg.appspot.com/true_recs/"+file+" "+path).read()
 
+#check if there are keywords to be deleted in local true_recs folder
+for file in current_files:
+	if(not (file in fb_files)):
+		os.remove(path+"/"+file)
 
 files=os.listdir(path)
 true_set = []	# list of keywords
