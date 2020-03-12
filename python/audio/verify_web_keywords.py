7626373a674835ec36906fd7e0ebe43c6c0da8eb
Luca Bertelli <luca.berte98@hotmail.it>
2020-03-12 21:27:00 +0100

    remove deleted keywords
    remove from the local true_recs folder keywords that are not present in the Firebase bucket anymore

--- python/audio/verify_web_keywords.py
+++ python/audio/verify_web_keywords.py
@@ -12,21 +12,29 @@ import time
 # np.set_printoptions(threshold = sys.maxsize) #print the whole array
 
 
-# download all .wav files from firebase bucket into true_recs local folder if not already in true_recs directory
+#### download all .wav files from firebase bucket into true_recs local folder if not already in true_recs directory
 path="true_recs"
+current_files=os.listdir(path) #list of keywords currently in local true_recs folder
+
+# list files from Firebase bucket
 fb_files=os.popen("gsutil ls gs://fpgacademy-mlg.appspot.com/true_recs/").read()
 fb_files=fb_files.replace("gs://fpgacademy-mlg.appspot.com/true_recs/","")
 fb_files=fb_files.split('\n')
 fb_files.remove('')
 fb_files.remove('')
 print("Number of Firebase keywords:",len(fb_files))
-ex_files=os.listdir(path)
+
+#check if there are new keywords to be downloaded from Firebase
 for file in fb_files:
-	if(not(file in ex_files)):
+	if(not(file in current_files)):
 		os.popen("gsutil cp -r gs://fpgacademy-mlg.appspot.com/true_recs/"+file+" "+path).read()
 
+#check if there are keywords to be deleted in local true_recs folder
+for file in current_files:
+	if(not (file in fb_files)):
+		os.remove(path+"/"+file)
 
-files=os.listdir(path)
+files=os.listdir(path) #new list of files
 
 print("Number of local keywords:", len(files))
 print("\n")
