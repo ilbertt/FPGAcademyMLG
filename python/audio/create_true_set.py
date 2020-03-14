213558a5de705eaad8191d9d8cea673d63d19370
Luca Bertelli <luca.berte98@hotmail.it>
2020-03-14 19:09:49 +0100

    new threshold procedure

--- python/audio/create_true_set.py
+++ python/audio/create_true_set.py
@@ -3,40 +3,29 @@ import os
 import numpy as np
 import functions	# our custom functions from functions.py
 
-threshold = 1000	# set threshold for keyword resize [wav sample amplitude]
+threshold = 10000	# set threshold for keyword resize [wav sample amplitude]
 fs = 44100			# sample rate
 length = 22000		# significant number of samples for each keyword
+maxScale = 30000	# normalizing scale
+goBackSize = 2000	# significant interval before and after the maximum peak
 
-#### download all .wav files from firebase bucket into true_recs local folder if not already in true_recs directory
 path="true_recs"
-current_files=os.listdir(path) #list of keywords currently in local true_recs folder
-
-# list files from Firebase bucket
-fb_files=os.popen("gsutil ls gs://fpgacademy-mlg.appspot.com/true_recs/").read()
-fb_files=fb_files.replace("gs://fpgacademy-mlg.appspot.com/true_recs/","")
-fb_files=fb_files.split('\n')
-fb_files.remove('')
-fb_files.remove('')
-
-#check if there are new keywords to be downloaded from Firebase
-for file in fb_files:
-	if(not(file in current_files)):
-		os.popen("gsutil cp -r gs://fpgacademy-mlg.appspot.com/true_recs/"+file+" "+path).read()
-
-#check if there are keywords to be deleted in local true_recs folder
-for file in current_files:
-	if(not (file in fb_files)):
-		os.remove(path+"/"+file)
-
 files=os.listdir(path)
+
 true_set = []	# list of keywords
+
 for file in files:
 	f, keyword = wav.read(path+"/"+file) 
 	keyword = functions.resample(keyword, f, fs)	# resample keyword to 44100 hz
-	keyword = functions.threshold_resize(keyword, threshold, length)	# keep only significant samples of the keyword
-	if len(keyword) != 1:	# threshold_resize returns array [0] if keyword is not valid
-		keyword = functions.resample(keyword, fs, fs/4)		# resample keyword to 11025 hz
-	true_set.append(keyword)	# append keywords to list
+	
+	keyword = functions.normalize(keyword, maxScale)	# normalize
+	#keyword = keyword[(peakIndex - padding) : (peakIndex + padding)]	# exctract significant samples of the keyword
+
+	keyword = functions.threshold_resize(keyword, threshold, length, goBackSize)	# keep only significant samples of the keyword
+	keyword = functions.resample(keyword, fs, fs/4)		# resample keyword to 11025 hz
+	'''if len(keyword) != 1:	# threshold_resize returns array [0] if keyword is not valid
+		keyword = functions.resample(keyword, fs, fs/4)		# resample keyword to 11025 hz'''
+	true_set.append(keyword)	# append keyword to list
 
 true_set = np.array(true_set)
 np.save("true_set", true_set)
--- python/audio/functions.py
+++ python/audio/functions.py
@@ -1,17 +1,70 @@
+import sounddevice as sd
+import time
+import matplotlib.pyplot as plt 
+
 import numpy as np
 from scipy import signal as sg
 
 # resizes "sample" to "lenght" starting from index of the first element above "threshold"
-def threshold_resize(sample, threshold, length):
+def threshold_resize(sample, threshold, length, goBackSize):
 	arr=list(map(lambda a: True if a>threshold else False, sample))
-	th_index=arr.index(True)
-	if (th_index<len(sample)-length):
-		sample=sample[th_index:th_index+length]
-		return sample
+	th_index=arr.index(True)	# first point above threshold is at thi index
+	if (th_index<len(sample)-length):	# if "th_index" is near the end of sample
+		start = th_index-goBackSize
+		if(start >= 0):	# if "th_index" is after "goBackSize"
+			sample = sample[start:start+length]
+		else:
+			sample = sample[0:length]
 	else:
-		return [0]
+		sample = sample[th_index:]
+		padding = np.zeros(length-len(sample))	# 0 padding to meet desired "length" of sample
+		sample = np.append(sample,padding)
+	return sample
 
-#change sample rate of "sample" from its "f" to new "fs"
+# change sample rate of "sample" from its "f" to new "fs"
 def resample(sample, f, fs):
 	sample=sg.resample(sample, int(len(sample)/(f/fs)))
 	return sample
+
+# normalize "keyword" to "maxScale" scale
+# return keyword and index of maximum peak
+def normalize(keyword, maxScale):
+	maxPeak = np.amax(np.absolute(keyword))	# maximum peak of keyword
+	keyword = keyword*(maxScale/maxPeak) # normalize using maxScale proportion
+
+	return keyword
+
+# test keyword in three steps:
+# - play keyword
+# - show samples array
+# - show FFT
+def test(keyword, f, threshold, title):
+	samples = keyword.size
+
+	# play .wav file
+	keyword = np.array(keyword, dtype='int16')
+	sd.play(keyword, f)
+
+	# plot .wav file
+	plt.subplot(211)
+	plt.xlabel("sample")
+	plt.ylabel("sound amplitude")
+	plt.plot(keyword)
+	plt.plot(np.zeros(samples)+threshold, 'k-', lw=2)	# threshold line 
+	plt.title(title)
+	plt.grid()
+	# plot fft
+	KEY = np.fft.fft(keyword)
+	KEY = KEY[:int(len(KEY)/2)]
+	timestep = 1/f
+	freq = np.fft.fftfreq(samples, d=timestep)
+	freq = freq[:int(len(freq)/2)]
+	plt.subplot(212)
+	plt.xlabel("frequency [Hz]")
+	plt.ylabel("spectral density")
+	plt.plot(freq, np.absolute(KEY))
+	plt.yscale("log")
+	plt.show()
+	
+	time.sleep(2)
+	return
