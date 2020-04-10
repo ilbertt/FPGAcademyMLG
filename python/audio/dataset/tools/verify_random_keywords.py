74d90d4b6c9b0f12701728de44439e285d413650
Luca Bertelli <luca.berte98@hotmail.it>
2020-04-10 22:23:41 +0200

    more log

--- python/audio/dataset/tools/verify_random_keywords.py
+++ python/audio/dataset/tools/verify_random_keywords.py
@@ -22,9 +22,11 @@ true_set = np.load('./../arrays/true_set.npy')
 
 ### check 20 random keywords from true_set
 rand_index = np.random.randint(0, len(true_set), num_random)	# generate 20 random indexes
+print(rand_index)
 ## play and plot each random keyword
 for kw_index in rand_index:
     kw = np.array(true_set[kw_index], dtype='int16')
     sd.play(kw, fs)
-    functions.test(kw, fs, threshold, "#: "+str(kw_index))
     print("#:", kw_index, ", len:", len(kw))
+    functions.test(kw, fs, threshold, "#: "+str(kw_index))
+    
