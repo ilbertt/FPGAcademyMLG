ac92d84dc21638d996636337fa2c3d554eee7e8b
gigo333 <gigo.bon@mail.com>
2020-04-10 19:02:36 +0200

    bug fixed

--- python/audio/neural_net/sw_neural_net_freq.py
+++ python/audio/neural_net/sw_neural_net_freq.py
@@ -11,8 +11,6 @@ l=len(t_setx[0])
 t_setx=np.round(t_setx/((2**10)/np.abs(t_setx).max()))
 t_setx=np.abs(np.fft.fft(t_setx))[:,int(l*7/8):]
 #t_setx=np.abs(np.fft.fftshift(np.fft.fft(t_setx))[:,0:int(l/2)])
-t_setx=t_setx.reshape((len(t_setx),1,-1))
-t_sety=t_sety.reshape((len(t_sety),1,-1))
 v_setx=t_setx[:n]
 t_setx=t_setx[n:]
 v_sety=t_sety[:n]
@@ -35,7 +33,7 @@ model=keras.Sequential([
 ])
 model.summary()
 model.compile(optimizer=keras.optimizers.SGD(lr=0.1),loss="binary_crossentropy",metrics=['accuracy'])
-model.fit(x=t_setx, y=t_sety,validation_split=0.3, epochs=1000, batch_size=64)
+model.fit(x=t_setx, y=t_sety,validation_split=0.3, epochs=10, batch_size=64)
 model.evaluate(v_setx, v_sety)
 np.save("weights", np.array(model.get_weights()))
 #layer_outputs = [layer.output for layer in model.layers]
