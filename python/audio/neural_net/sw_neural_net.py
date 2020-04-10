9f2adc50892495c390db25f32f2058f2501af479
Luca Bertelli <luca.berte98@hotmail.it>
2020-04-10 22:32:56 +0200

    little improvements

--- python/audio/neural_net/sw_neural_net.py
+++ python/audio/neural_net/sw_neural_net.py
@@ -1,8 +1,23 @@
+### uncomment the following lines if you have GPU but you want to use only CPU
+#import os
+#os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
 import tensorflow as tf
+
+### comment the following lines if you don't have GPU 
+# from here
+gpus= tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(gpus))
+tf.config.experimental.set_memory_growth(gpus[0], True)
+# to here
+
 from tensorflow import keras
 import numpy as np
 from keras import backend as K
 from keras import models
+
+N = 100	# lenght of validation set
+epochs = 10
 
 t_setx, t_sety=np.load("./../dataset/arrays/dataset.npy", allow_pickle=True)
 
@@ -10,6 +25,12 @@ t_setx=np.vstack(t_setx).astype(np.float)
 t_setx=t_setx.reshape((len(t_setx),1,1,-1))
 t_sety=np.vstack(t_sety).astype(np.float)
 t_sety=t_sety.reshape((len(t_sety),1,1,-1))
+
+v_setx = t_setx[len(t_setx)-N:]
+v_sety = t_sety[len(t_sety)-N:]
+
+t_setx = t_setx[:len(t_setx)-N]
+t_sety = t_sety[:len(t_sety)-N]
 
 print(t_setx[0].shape)
 print(t_sety[0].shape)
@@ -19,21 +40,27 @@ def custom_activation(x):
 
 model=keras.Sequential([
     keras.layers.Conv2D(16,1, input_shape=t_setx[0].shape),
+
 	keras.layers.Dense(16,activation=tf.nn.sigmoid),
+
 	keras.layers.Dense(8,activation=tf.nn.sigmoid),
+
 	keras.layers.Dense(4,activation=tf.nn.sigmoid),
+
 	keras.layers.Dense(2,activation=tf.nn.sigmoid),
+
 	keras.layers.Dense(1, activation=tf.nn.sigmoid)
 ])
 model.summary()
-model.compile(optimizer=tf.optimizers.Adam(),loss="binary_crossentropy")
+model.compile(optimizer=tf.optimizers.RMSprop(lr=0.01),loss="binary_crossentropy",metrics=['accuracy'])
+#model.compile(optimizer=tf.optimizers.Adam(),loss="binary_crossentropy")
 
-model.fit(x=t_setx, y=t_sety, epochs=100)
+model.fit(x=t_setx, y=t_sety, validation_split= 0.3, epochs=epochs)
+model.evaluate(v_setx, v_sety)
+
 np.save("tensorflow_files/weights", np.array(model.get_weights()))
-#layer_outputs = [layer.output for layer in model.layers]
-#activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
-#activations = activation_model.predict(t_setx[0])
 model.save("tensorflow_files/model.h5") #to open the model use: model=keras.models.load_model("model.h5")
-print(model.predict([[t_setx[0]]]))
-print(t_sety[0])
+
+print(model.predict([[v_setx[0]]]))
+print(v_sety[0])
 
--- python/audio/neural_net/sw_neural_net_freq.py
+++ python/audio/neural_net/sw_neural_net_freq.py
@@ -1,23 +1,37 @@
+### uncomment the following lines if you have GPU but you want to use only CPU
+#import os
+#os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+
 import tensorflow as tf
+
+### comment the following lines if you don't have GPU 
+# from here
+gpus= tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(gpus))
+tf.config.experimental.set_memory_growth(gpus[0], True)
+# to here
+
 from tensorflow import keras
 import numpy as np
 from keras import backend as K
 from keras import models
 
-n=100
-t_setx, t_sety=np.load("dataset.npy", allow_pickle=True)
+N = 100	# lenght of validation set
+epochs = 10000
+
+t_setx, t_sety=np.load("./../dataset/arrays/dataset.npy", allow_pickle=True)
 t_setx=np.vstack(t_setx).astype(np.float)
 l=len(t_setx[0])
-t_setx=np.round(t_setx/((2**10)/np.abs(t_setx).max()))
-t_setx=np.abs(np.fft.fft(t_setx))[:,int(l*7/8):]
-#t_setx=np.abs(np.fft.fftshift(np.fft.fft(t_setx))[:,0:int(l/2)])
-v_setx=t_setx[:n]
-t_setx=t_setx[n:]
-v_sety=t_sety[:n]
-t_sety=t_sety[n:]
+t_setx=np.round(t_setx/((2**10)/np.abs(t_setx).max())) # mapped on 10 bit
+t_setx=np.abs(np.fft.fft(t_setx))[:,int(l/2):]#[:,int(l*7/8):]
+
+v_setx=t_setx[:N]
+t_setx=t_setx[N:]
+v_sety=t_sety[:N]
+t_sety=t_sety[N:]
 t_sety=np.vstack(t_sety).astype(np.float)
-print(t_setx[0].shape)
-print(t_sety[0].shape)
+print(len(t_setx), t_setx[0].shape)
+print(len(t_sety), t_sety[0].shape)
 
 #def custom_activation(x):
     #return tf.nn.sigmoid(x,max_value=5000)
@@ -33,11 +47,13 @@ model=keras.Sequential([
 ])
 model.summary()
 model.compile(optimizer=keras.optimizers.SGD(lr=0.1),loss="binary_crossentropy",metrics=['accuracy'])
-model.fit(x=t_setx, y=t_sety,validation_split=0.3, epochs=10, batch_size=64)
+model.fit(x=t_setx, y=t_sety,validation_split=0.3, epochs=epochs, batch_size=64)
+
+v_setx = np.asarray(v_setx).astype(np.float)
+v_sety = np.asarray(v_sety).astype(np.float)
 model.evaluate(v_setx, v_sety)
-np.save("weights", np.array(model.get_weights()))
-#layer_outputs = [layer.output for layer in model.layers]
-#activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
-#activations = activation_model.predict(t_setx[0])
-print(model.predict([[t_setx[0]]]))
-print(t_sety[0])
+
+np.save("tensorflow_files/weights_freq", np.array(model.get_weights()))
+model.save("tensorflow_files/model_freq.h5") #to open the model use: model=keras.models.load_model("model.h5")
+print(model.predict([[v_setx[0]]]))
+print(v_sety[0])
